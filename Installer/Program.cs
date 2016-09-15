7f2f7da0c82f3d8ec01e8b18a7aab13d91cc939d
Bardia Jedi <bardiajedi@gmail.com>
2016-09-15 15:14:14 +0200

    Can get Ctx now!

---
+++ Installer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+
+namespace Installer {
+	class Program {
+		static void Main(string[] args) {
+			using (ClientContext ctx = GetAppOnlyCtx(new Uri("https://luviz.sharepoint.com/sites/OfficeDev2"))) {
+				
+			}
+			Console.ReadLine();
+		}
+
+		public static ClientContext GetAppOnlyCtx(Uri siteUri) {
+			string realm = TokenHelper.GetRealmFromTargetUrl(siteUri);
+			var token = TokenHelper.GetAppOnlyAccessToken(TokenHelper.SharePointPrincipal, siteUri.Authority, realm).AccessToken;
+			return TokenHelper.GetClientContextWithAccessToken(siteUri.ToString(), token);
+		}
+	}
+}
